56666a65772afa3741f9b0a8546e6131a97ed81e
tobygundesen <toby.gundesen@danone.com>
2026-09-23 17:47:25 +1200

    Fix Vite base path for GitHub Pages preview

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/E3-Application/',
+  base: '/E3-Application-v2/',
   plugins: [react()],
 });
